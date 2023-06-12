43ccaf90e0c39c9c57f6a443a531c657f233ad78
Maduchris <o.madubuike@alustudent.com>
2023-06-12 03:53:34 -0700

    7-multi_c.js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (argNum) {
+  for (let a = 0; a < argNum; a+++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
