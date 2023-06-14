9487ea1207b13e51831655c4a5bd7a03fbff5019
Maduchris <o.madubuike@alustudent.com>
2023-06-14 05:32:02 -0700

    3-rectangle.js

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(W)) > 0 && (h == parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.widht));
+  }
+}
+module.exports = Rectangle;
