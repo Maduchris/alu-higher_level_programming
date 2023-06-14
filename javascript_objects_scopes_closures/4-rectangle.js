c79adb0bb115380effdd1bcb306f4d7520d061b6
Maduchris <o.madubuike@alustudent.com>
2023-06-14 06:46:45 -0700

    4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -13,7 +13,7 @@ module.exports = class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let output = '';
       for (let j = 0; j < this.width; j++) {
-	output += 'X';
+        output += 'X';
       }
       console.log(output);
     }
